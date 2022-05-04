ae17c482a6511e08c996d441c879db63ea2bb64c
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2022-05-04 08:08:12 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ variable "length" {
   type = string
 }
 
-variable "length2" {
-  type = string
-}
+# variable "length2" {
+#   type = string
+# }
 
  resource "random_pet" "name44" {
   length    = var.length
